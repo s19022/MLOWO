a48e834b31d216cfcae7a250d97175bf990cab11
Volodymyr Dovhyi <s19022@pjwstk.edu.pl>
2021-01-14 21:56:12 +0200

    implemented add Subscription method

--- Mlowo/src/com/company/Subscription/Subscription.java
+++ Mlowo/src/com/company/Subscription/Subscription.java
@@ -3,23 +3,24 @@ package com.company.Subscription;
 import com.company.Client;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.UUID;
 
 public class Subscription {
+    private static HashMap<Subscription, Client> subscriptions = new HashMap<>();
 
     private final String subscriptionId;
     private TypeOfSubscription typeOfSubscription;
     private long price;
     private final LocalDateTime startDate;
     private LocalDateTime expireDate;
-    private final Client client;
+    private Client client;
 
-    private Subscription(TypeOfSubscription typeOfSubscription, long price, LocalDateTime startDate, LocalDateTime expireDate, Client client) {
+    private Subscription(TypeOfSubscription typeOfSubscription, long price, LocalDateTime startDate, LocalDateTime expireDate) {
         subscriptionId = UUID.randomUUID().toString();
         this.typeOfSubscription = typeOfSubscription;
         this.startDate = startDate;
         this.expireDate = expireDate;
-        this.client = client;
     }
 
     public String getSubscriptionId() {
@@ -57,6 +58,16 @@ public class Subscription {
         this.expireDate = expireDate;
     }
 
+    public boolean setClient(Client client){
+        if (client == null)  return false;
+        if(subscriptions.containsKey(this)){
+            if (subscriptions.get(this).equals(client)) return true;
+            return false;
+        }
+        this.client = client;
+        return true;
+    }
+
     public void checkStatus(){}
 
     public boolean isExpired(){
@@ -64,14 +75,13 @@ public class Subscription {
     }
 
     public static Subscription getInstance(TypeOfSubscription typeOfSubscription, double price,
-                                           LocalDateTime startDate, LocalDateTime expireDate, Client client) throws SubscriptionInitializationException {
+                                           LocalDateTime startDate, LocalDateTime expireDate) throws SubscriptionInitializationException {
         if (startDate == null) throw new SubscriptionInitializationException("start date is null");
         if (expireDate == null) throw new SubscriptionInitializationException("expire date is null");
         if (expireDate.isBefore(startDate)) throw new SubscriptionInitializationException("expire date is smaller than start date");
         if (price < 0) throw new SubscriptionInitializationException("price should be a non-negative number");
         if (typeOfSubscription == null) throw new SubscriptionInitializationException("type of subscription is null");
-        if (client == null) throw new SubscriptionInitializationException("Client is null");
-        
-        return new Subscription(typeOfSubscription, (long)(price * 100), startDate, expireDate, client);
+
+        return new Subscription(typeOfSubscription, (long)(price * 100), startDate, expireDate);
     }
 }
