9f54d8a7638cf5ad8140fd534eb95355dbbbb897
Fedir Studinskyi <s19428@pjwstk.edu.pl>
2021-01-14 20:01:39 +0200

    key constraint in Bracelet

--- Mlowo/src/com/company/Bracelet.java
+++ Mlowo/src/com/company/Bracelet.java
@@ -1,21 +1,44 @@
-package com.company;
+
 
 import java.awt.*;
+import java.util.UUID;
+import java.util.*;
 
 public class Bracelet {
-    private int braceletId;
+    private String braceletId;
     private boolean isActive;
-    private Color color;
+    private Color color; 
 
-    public Bracelet(int braceletId, boolean isActive, Color color) {
-        this.braceletId = braceletId;
+    private static HashMap<String, Bracelet> bracelets = new HashMap<String, Bracelet>();
+
+    public Bracelet(boolean isActive, Color color) {
         this.isActive = isActive;
         this.color = color;
+
+        String uniqueKey = UUID.randomUUID().toString();
+        switch(bracelets.containsKey(uniqueKey) + "")
+        {
+            case "false":
+                    this.braceletId = uniqueKey;
+                bracelets.put(uniqueKey, this);
+            break;
+            case "true": // is it even possible???, probably not
+                System.out.println("Generated id is not unique");
+            break;
+        }
     }
+
+    public static void showAllBracelets()
+    {
+        for (String key: bracelets.keySet()) {
+            System.out.println(bracelets.get(key));
+        }
+    }
+
     public void measureHeartRate(){}
     public void sendHelpSignal(){}
 
-    public int getBraceletId() {
+    public String getBraceletId() {
         return braceletId;
     }
 
