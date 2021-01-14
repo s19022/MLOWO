60ad85fee5b06993b7c7fdc6a776d77ea7bddd41
Fedir Studinskyi <s19428@pjwstk.edu.pl>
2021-01-14 21:11:15 +0200

    bracelet-client relation

--- Mlowo/src/com/company/Main.java
+++ Mlowo/src/com/company/Main.java
@@ -1,15 +1,39 @@
 package com.company;
 
 import java.awt.*;
+import java.util.Date;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
 
-        Bracelet b1 = new Bracelet(true, new Color(200, 255, 255));
-        Bracelet b2 = new Bracelet(true, new Color(200, 255, 255));
+        {
+            Bracelet b1 = new Bracelet(true, new Color(200, 255, 255));
+            Bracelet b2 = new Bracelet(true, new Color(200, 255, 255));
+            Bracelet.showAllBracelets();
 
-        Bracelet.showAllBracelets();
+            Client c1 = new Client(
+                    "firstNmae",
+                    "lastName",
+                    new Date(),
+                    "myEmail@mail.com",
+                    "PhoneNumber",
+                    "password"
+            );
+            Client c2 = new Client(
+                    "firstNmae",
+                    "lastName",
+                    new Date(),
+                    "myEmail@mail.com",
+                    "PhoneNumber",
+                    "password"
+            );
+
+            c1.addBracelet(b1);
+            c1.addBracelet(b1);
+            c2.addBracelet(b1);
+            c2.addBracelet(b2);
+        }
     }
 }
