785efd3dd602535f86e3a117d6e8379a68f6f953
Volodymyr Dovhyi <s19022@pjwstk.edu.pl>
2021-01-15 15:55:02 +0200

    added getters and setters

--- Mlowo/src/com/company/HelpRecord/HelpRecord.java
+++ Mlowo/src/com/company/HelpRecord/HelpRecord.java
@@ -1,15 +1,23 @@
 package com.company.HelpRecord;
 
+import com.company.Client;
+import com.company.FirstResponder;
+
 import java.time.LocalDateTime;
 
 public class HelpRecord {
     private Outcome outcome;
     private final String location;
     private final LocalDateTime dateTime;
-    public HelpRecord( LocalDateTime dateTime, Outcome outcome, String location) {
-        this.dateTime = dateTime;
+    private final Client client;
+    private final FirstResponder firstResponder;
+
+    public HelpRecord(Outcome outcome, String location, LocalDateTime dateTime, Client client, FirstResponder firstResponder) {
         this.outcome = outcome;
         this.location = location;
+        this.dateTime = dateTime;
+        this.client = client;
+        this.firstResponder = firstResponder;
     }
 
     private Outcome getOutcome(){
@@ -22,5 +30,18 @@ public class HelpRecord {
 
     public String getLocation() {
         return location;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setOutcome(Outcome outcome) {
+        if (outcome == null) return;
+        this.outcome = outcome;
+    }
+
+    public FirstResponder getFirstResponder() {
+        return firstResponder;
     }
 }
