18da48622af538fa29449b3886b925bc3eba8aa4
Volodymyr Dovhyi <s19022@pjwstk.edu.pl>
2021-01-15 14:01:40 +0200

    added JUnit

---
+++ Mlowo/Tests/com/company/Subscription/SubscriptionTest.java
@@ -0,0 +1,52 @@
+package com.company.Subscription;
+
+public class SubscriptionTest {
+
+    @org.junit.Test
+    public void getSubscriptionId() {
+    }
+
+    @org.junit.Test
+    public void getTypeOfSubscription() {
+    }
+
+    @org.junit.Test
+    public void getPrice() {
+    }
+
+    @org.junit.Test
+    public void setTypeOfSubscription() {
+    }
+
+    @org.junit.Test
+    public void setPrice() {
+    }
+
+    @org.junit.Test
+    public void getStartDate() {
+    }
+
+    @org.junit.Test
+    public void getExpireDate() {
+    }
+
+    @org.junit.Test
+    public void setExpireDate() {
+    }
+
+    @org.junit.Test
+    public void setClient() {
+    }
+
+    @org.junit.Test
+    public void checkStatus() {
+    }
+
+    @org.junit.Test
+    public void isExpired() {
+    }
+
+    @org.junit.Test
+    public void getInstance() {
+    }
+}
