33ae2fd6a2f4bab1a7450e407c3f73de7fa20d28
Volodymyr Dovhyi <s19022@pjwstk.edu.pl>
2021-01-15 15:48:00 +0200

    moved to HelpRecord folder

--- Mlowo/src/com/company/HelpRecord/HelpRecord.java
+++ Mlowo/src/com/company/HelpRecord/HelpRecord.java
@@ -1,4 +1,4 @@
-package com.company;
+package com.company.HelpRecord;
 
 import java.time.LocalDateTime;
 
--- Mlowo/src/com/company/HelpRecord/Outcome.java
+++ Mlowo/src/com/company/HelpRecord/Outcome.java
@@ -1,4 +1,4 @@
-package com.company;
+package com.company.HelpRecord;
 
 public enum Outcome {
     POSITIVE,
