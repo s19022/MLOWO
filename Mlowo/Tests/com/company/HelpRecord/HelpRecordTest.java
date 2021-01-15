fd0e4c0d133c683fa8e4e5b58c2581dd9333384a
Volodymyr Dovhyi <s19022@pjwstk.edu.pl>
2021-01-15 16:36:02 +0200

    imlemented test, fixed bug

--- Mlowo/Tests/com/company/HelpRecord/HelpRecordTest.java
+++ Mlowo/Tests/com/company/HelpRecord/HelpRecordTest.java
@@ -1,29 +1,72 @@
 package com.company.HelpRecord;
 
 
+import com.company.Client;
+import com.company.FirstResponder;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+
+import static org.junit.Assert.assertEquals;
 
 public class HelpRecordTest {
 
-    HelpRecord helpRecord;
+    private HelpRecord helpRecord;
+    private LocalDateTime time;
+    private Client client;
+    private FirstResponder firstResponder;
+
+    @Before
+    public void before(){
+        time = LocalDateTime.of(2021, Month.JANUARY, 6, 23, 58);
+        firstResponder = new FirstResponder("Joe", "Brown", LocalDateTime.of(2000, Month.MARCH, 22, 12, 34),
+                "myEmail@mail.com",
+                "PhoneNumber",
+                "password");
+
+        client = new Client(
+                "firstName",
+                "lastName",
+                LocalDateTime.of(2001, Month.FEBRUARY, 8, 13,45),
+                "myEmail@mail.com",
+                "PhoneNumber",
+                "password",
+                "platform 9,75");
+
+
+        helpRecord = new HelpRecord(Outcome.NEGATIVE, "loc1", time, client, firstResponder);
+
+    }
 
     @Test
     public void getDateTime() {
+        assertEquals(time, helpRecord.getTime());
     }
 
     @Test
     public void getLocation() {
+        assertEquals("loc1", helpRecord.getLocation());
     }
 
     @Test
     public void getClient() {
+        assertEquals(client, helpRecord.getClient());
     }
 
     @Test
     public void setOutcome() {
+        helpRecord.setOutcome(null);
+        assertEquals(Outcome.NEGATIVE, helpRecord.getOutcome());
+
+        helpRecord.setOutcome(Outcome.POSITIVE);
+        assertEquals(Outcome.POSITIVE, helpRecord.getOutcome());
+
     }
 
     @Test
     public void getFirstResponder() {
+        assertEquals(firstResponder, helpRecord.getFirstResponder());
     }
 }
--- Mlowo/src/com/company/HelpRecord/HelpRecord.java
+++ Mlowo/src/com/company/HelpRecord/HelpRecord.java
@@ -8,24 +8,24 @@ import java.time.LocalDateTime;
 public class HelpRecord {
     private Outcome outcome;
     private final String location;
-    private final LocalDateTime dateTime;
+    private final LocalDateTime time;
     private final Client client;
     private final FirstResponder firstResponder;
 
     public HelpRecord(Outcome outcome, String location, LocalDateTime dateTime, Client client, FirstResponder firstResponder) {
         this.outcome = outcome;
         this.location = location;
-        this.dateTime = dateTime;
+        this.time = dateTime;
         this.client = client;
         this.firstResponder = firstResponder;
     }
 
-    private Outcome getOutcome(){
+    public Outcome getOutcome(){
         return outcome;
     }
 
-    public LocalDateTime getDateTime() {
-        return dateTime;
+    public LocalDateTime getTime() {
+        return time;
     }
 
     public String getLocation() {
