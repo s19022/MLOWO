94f1dfdaf917b9ebf9b449ef6eb5a03d4298d4a1
s19312 <s19312@pjwstk.edu.pl>
2021-01-16 19:22:11 +0200

    MlowoBike class implemented

--- Mlowo/src/com/company/MlowoBike.java
+++ Mlowo/src/com/company/MlowoBike.java
@@ -1,16 +1,45 @@
 package com.company;
 
-public class MlowoBike {
-    private int bikeId;
+import java.util.HashMap;
+import java.util.UUID;
 
-    public MlowoBike(int bikeId) {
-        this.bikeId = bikeId;
+public class MlowoBike {
+    private final String bikeId;
+    private boolean isLocked;
+    private MedicalKit medicalKit;
+
+    private static HashMap<String,MlowoBike> mlowoBikes = new HashMap<>();
+
+    public MlowoBike() {
+        bikeId = UUID.randomUUID().toString();
+        mlowoBikes.put(bikeId, this);
     }
 
-    public int getBikeId() {
+    public static void showAllMlowoBikes()
+    {
+        for (String key: mlowoBikes.keySet()) {
+            System.out.println(mlowoBikes.get(key));
+        }
+    }
+    public MedicalKit getMedicalKit() {
+        return medicalKit;
+    }
+
+    public void setMedicalKit(MedicalKit medicalKit) {
+        this.medicalKit = medicalKit;
+    }
+
+    public String getBikeId() {
         return bikeId;
     }
+    public boolean getLockStatus(){
+        return isLocked;
+    }
 
-    public void Unlock(){}
-    public void Lock(){}
+    public void Unlock(){
+        this.isLocked = false;
+    }
+    public void Lock(){
+        this.isLocked = true;
+    }
 }
