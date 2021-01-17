f895478e9236f962033d024d191cbc085a58aed0
Volodymyr Dovhyi <s19022@pjwstk.edu.pl>
2021-01-17 11:18:49 +0200

    initialized class

---
+++ Mlowo/Tests/com/company/ClientTest.java
@@ -0,0 +1,37 @@
+package com.company;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ClientTest {
+
+    @Before
+    public void setUp(){
+        
+    }
+
+    @Test
+    public void getHomeAddress() {
+    }
+
+    @Test
+    public void isBlocked() {
+    }
+
+    @Test
+    public void setBlocked() {
+    }
+
+    @Test
+    public void getBracelets() {
+    }
+
+    @Test
+    public void addBracelet() {
+    }
+
+    @Test
+    public void addSubscription() {
+    }
+}
