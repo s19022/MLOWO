d899901f8499085776485cdbd163d82c13d9848c
Volodymyr Dovhyi <s19022@pjwstk.edu.pl>
2021-01-20 18:21:04 +0200

    changed Date to LocalDateTime

--- Mlowo/src/com/company/MedicalCard.java
+++ Mlowo/src/com/company/MedicalCard.java
@@ -1,16 +1,17 @@
 package com.company;
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.UUID;
 
 public class MedicalCard {
     private final String medicalCardId;
-    private Date expirationDate;
+    private LocalDateTime expirationDate;
     private String Owner;
     public static HashMap<String,MedicalCard> medicalCards = new HashMap<>();
 
 
-    public MedicalCard(String medicalCardId, Date expirationDate, String owner) {
+    public MedicalCard(String medicalCardId, LocalDateTime expirationDate, String owner) {
         this.medicalCardId = UUID.randomUUID().toString();
         this.expirationDate = expirationDate;
         Owner = owner;
