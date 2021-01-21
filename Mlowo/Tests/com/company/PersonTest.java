815d71f3257a721613923056f9cf20742ac08612
s18289 <s18289@pjwstk.edu.pl>
2021-01-21 13:10:11 +0100

    added PersonTest

---
+++ Mlowo/Tests/com/company/PersonTest.java
@@ -0,0 +1,54 @@
+import static org.junit.jupiter.api.Assertions.*;
+import com.company.Person;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+class PersonTest
+{
+    @org.junit.jupiter.api.Test
+    void getLastName()
+    {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void getDateOfBirth()
+    {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getEmail()
+    {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getPhoneNumber()
+    {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getPassword()
+    {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getFirstName()
+    {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testEquals()
+    {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testHashCode()
+    {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addHelpRecord()
+    {
+    }
+}
